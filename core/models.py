feca6a4ec525ab72203f8970bd9f6b7d1a5abfc7
gabriellpedro <gabriel.pedro47@hotmail.com>
2023-06-14 21:15:29 -0300

    Adjusting models and including migrations

--- core/models.py
+++ core/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+from core.enums import CURSOS, PROFESSORES, SEMESTRES
+
 class Presenca(models.Model):
-    NomeAluno = models.CharField(verbose_name="Nome do Aluno", max_length=100)
-    NomeProfessor = models.CharField(verbose_name="Nome do Professor", max_length=100)
-    Semestre = models.IntegerField(verbose_name="Semestre")
-    NomeCurso = models.CharField(verbose_name="Nome do Curso", max_length=20)
-    DataPresenca = models.DateField(verbose_name="Data de Presença")
+    NomeAluno = models.CharField(verbose_name="Nome_do_Aluno", max_length=100)
+    NomeProfessor = models.IntegerField(verbose_name='Nome_do_Professor', choices=PROFESSORES)
+    Semestre = models.IntegerField(verbose_name="Semestre", choices=SEMESTRES)
+    NomeCurso = models.IntegerField(verbose_name='Nome_do_Curso', choices=CURSOS)
     
